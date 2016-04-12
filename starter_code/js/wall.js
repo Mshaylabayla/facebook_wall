654dcdf416dd34b75d245c97bc218f9b9fe05884
mshaylabayla <Mshaylabayla@gmail.com>
2016-04-12 20:22:56 +0000

    example comment

--- starter_code/js/wall.js
+++ starter_code/js/wall.js
@@ -1,6 +1,15 @@
 $(document).ready(function()
 {
-	$('#message').focus();
+	$("#post-btn").click(function() {
+		var message ="<br/><div class='row'><div class='col-md-2'><img src='http://cdn0.dailydot.com/uploaded/images/original/2014/8/18/pusheen.gif' width='50px'><div/>" +"<div class=' col-md-10'>";
+		message += "<h3>Shay allDay<h3/>";
+		message += $("#message").val().trim();
+		message += '</div></div>'
+		
+	$("#wall").prepend(message);
+	  
+      });
+      
 
 	// YOUR CODE GOES HERE
 });
